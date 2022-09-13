807de7027c1fde2a7190fc2ae4440423c59a8b5f
Станислав Микулич <staslz42stas@gmail.com>
2022-09-13 14:53:29 +0300

    AUTO FROM WORK 13.09.2022 14:53:28,72

--- up to inet - push.bat
+++ up to inet - push.bat
@@ -4,6 +4,6 @@ cd ..
 git add .
 git commit -m "AUTO FROM WORK %date% %time%"
 git config --global http.version HTTP/1.1
-git push 
+git push origin --force
 git config --global http.version HTTP/2
 pause
