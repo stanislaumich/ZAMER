0f7399cada1bd201c0ab110fffb89299e1228b84
stanislaumich <staslz42stas@gmail.com>
2022-10-13 21:33:27 +0300

    AUTO FROM WORK 13.10.2022 21:33:27,52

--- BACKUP/create_zamer.sql
+++ BACKUP/create_zamer.sql
@@ -5,6 +5,7 @@ startup restrict;
 ALTER DATABASE CHARACTER SET INTERNAL_USE AL32UTF8;
 shutdown immediate;
 startup;
+--alter session set "_ORACLE_SCRIPT"=true;
 drop user zamer cascade;
 create user zamer identified by zamer default tablespace users temporary tablespace temp;
 grant dba to zamer;
