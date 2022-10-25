03c7e0c0da7b6a5026f4a3955a777093b48c0ca6
Станислав Микулич <staslz42stas@gmail.com>
2022-10-25 12:55:24 +0300

    AUTO FROM WORK 25.10.2022 12:55:23,37

--- UPDATER/UMAin.pas
+++ UPDATER/UMAin.pas
@@ -9,18 +9,26 @@ uses
   FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
   FireDAC.Phys, FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef,
   FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteWrapper.Stat, FireDAC.VCLUI.Wait,
-  Data.DB, FireDAC.Comp.Client;
+  Data.DB, FireDAC.Comp.Client, FireDAC.Comp.UI, Vcl.Grids, FireDAC.Stan.Param,
+  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.Comp.DataSet, ustr, ShellApi;
 
 type
-  TFMain = class(TForm)
+  TFMainUpdater = class(TForm)
     GroupBox1: TGroupBox;
     BitBtn1: TBitBtn;
     BitBtn2: TBitBtn;
     BitBtn3: TBitBtn;
     Memo1: TMemo;
     FDC: TFDConnection;
-    FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink;
+    GroupBox2: TGroupBox;
+    StringGrid1: TStringGrid;
+    BitBtn4: TBitBtn;
+    QueryList: TFDQuery;
     procedure BitBtn3Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure BitBtn4Click(Sender: TObject);
+    function MakeOne(n:integer):boolean;
+    function MakeAll(fini:string):boolean;
   private
     { Private declarations }
   public
@@ -28,13 +36,24 @@ type
   end;
 
 var
-  FMain: TFMain;
+  FMainUpdater: TFMainUpdater;
+
+
+const
+ dbname='update.sqlite';
 
 implementation
 
 {$R *.dfm}
 
 uses UFile;
+
+
+procedure Log(s:string);
+ begin
+  FMainUpdater.Memo1.Lines.Add(s);
+ end;
+
 //----------------------------------------------------------------
 function GetInetFile(const fileURL, FileName: string): boolean;
 const
@@ -70,24 +89,98 @@ begin
   end;
 end;
 //----------------------------------------------------------------
-{
-if RenameFile('myfile.txt', 'newfile.txt') then
-  ShowMessage('Файл успешно переименован.')
-else
-  ShowMessage('Ошибка: файл не был переименован.');
-}
+
 //----------------------------------------------------------------
-function Makeone(fini:string):boolean;
+function TFMainUpdater.MakeOne(n:integer):boolean;
+ var
+  s:string;
+  fin:string;
+begin
+ //fin:=extractfilepath(paramstr(0))+datetostr(date)+strreplace(timetostr(time),':','-')+'.tmp';
+ fin:=extractfilepath(paramstr(0))+'tmp';
+ Log('Секция: ' + Stringgrid1.cells[0,n]);
+ Log('Скачиваем');
+
+ if GetInetFile(Stringgrid1.cells[1,n], fin) then
+  begin
+   Log('Файл загружен');
+  end
+ else
+  begin
+   Log('Не удалось загрузить файл');
+   Exit;
+  end;
+  //ShellExecute(Handle, 'open', PWideChar(Stringgrid1.cells[3,n]), nil, nil, SW_SHOWNORMAL);
+  if Stringgrid1.cells[3,n]<>'' then
+  begin
+   Log('Команда до обновления '+Stringgrid1.cells[3,n]);
+   ExecAndWait(PWideChar(Stringgrid1.cells[3,n]));
+  end
+   else
+  Log('Команды до обновления нет');
+  pause(1000);
+ if RenameFile(fin, Stringgrid1.cells[2,n]) then
+  Log('Обновлено.')
+ else
+  begin
+   Log('Ошибка обновления, возможно файл открыт?');
+   Exit;
+  end;
+ DeleteFile(fin);
+ if Stringgrid1.cells[4,n]<>'' then
+  begin
+   Log('Команда после обновления '+Stringgrid1.cells[4,n]);
+   ExecAndWait(PWideChar(Stringgrid1.cells[4,n]));
+  end
+ else
+  Log('Команды после обновления нет');
+ Log('Секция завершена');
+ Log('');
+end;
+//----------------------------------------------------------------
+function TFMainUpdater.MakeAll(fini:string):boolean;
 var
- Ini:tinifile;
+ n:integer;
 begin
 
 
 end;
 //----------------------------------------------------------------
-procedure TFMain.BitBtn3Click(Sender: TObject);
+procedure TFMainUpdater.BitBtn3Click(Sender: TObject);
 begin
  FFile.showmodal;
+end;
+
+procedure TFMainUpdater.BitBtn4Click(Sender: TObject);
+var
+ s: string;
+begin
+ MakeOne(Stringgrid1.row);
+end;
+
+procedure TFMainUpdater.FormCreate(Sender: TObject);
+var
+ i:integer;
+begin
+ Log('Открываю БД '+Extractfilepath(paramstr(0))+''+dbname);
+ Log('');
+ FDC.Params.Database:=Extractfilepath(paramstr(0))+''+dbname;
+ FDC.connected:=true;
+ Querylist.Open;
+ i:=1;
+ While not QueryList.Eof do
+  begin
+   Stringgrid1.cells[0,i]:=QueryList.FieldByName('nm').AsString;
+   Stringgrid1.cells[1,i]:=QueryList.FieldByName('url').AsString;
+   Stringgrid1.cells[2,i]:=QueryList.FieldByName('path').AsString;
+   Stringgrid1.cells[3,i]:=QueryList.FieldByName('before').AsString;
+   Stringgrid1.cells[4,i]:=QueryList.FieldByName('after').AsString;
+   QueryList.Next;
+   i:=i+1;
+   Stringgrid1.RowCount:=Stringgrid1.RowCount+1;
+  end;
+ Stringgrid1.RowCount:=Stringgrid1.RowCount-1;
+ FDC.connected:=false;
 end;
 
 end.
--- UPDATER/UPDATER.dpr
+++ UPDATER/UPDATER.dpr
@@ -2,15 +2,16 @@ program UPDATER;
 
 uses
   Vcl.Forms,
-  UMAin in 'UMAin.pas' {FMain},
-  UFile in 'UFile.pas' {FFile};
+  UMAin in 'UMAin.pas' {FMainUpdater},
+  UFile in 'UFile.pas' {FFile},
+  Ustr in '..\ZAMER\Ustr.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TFMain, FMain);
+  Application.CreateForm(TFMainUpdater, FMainUpdater);
   Application.CreateForm(TFFile, FFile);
   Application.Run;
 end.
