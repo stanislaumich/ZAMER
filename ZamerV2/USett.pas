c371d567cf3769da8088c68a03e8d728a79859b8
Станислав Микулич <staslz42stas@gmail.com>
2022-11-24 09:11:17 +0300

    AUTO FROM WORK 24.11.2022  9:11:16,78

--- ZamerV2/USett.pas
+++ ZamerV2/USett.pas
@@ -8,7 +8,8 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, FireDAC.Stan.Intf,
   FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
   FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt,
-  Data.DB, Vcl.Grids, Vcl.DBGrids, FireDAC.Comp.DataSet, FireDAC.Comp.Client;
+  Data.DB, Vcl.Grids, Vcl.DBGrids, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
+  Vcl.Buttons;
 
 type
   TFSett = class(TForm)
@@ -20,6 +21,21 @@ type
     DSStend: TDataSource;
     GridStend: TDBGrid;
     TablStend: TFDTable;
+    GroupBox3: TGroupBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    OpenDialog1: TOpenDialog;
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    QTemp: TFDQuery;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
+    procedure BitBtn2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -30,7 +46,32 @@ var
   FSett: TFSett;
 
 implementation
- //Uses uzv2Main;
+ Uses uzv2Main;
 {$R *.dfm}
+
+procedure TFSett.BitBtn1Click(Sender: TObject);
+begin
+
+ FSett.Close;
+end;
+
+procedure TFSett.BitBtn2Click(Sender: TObject);
+begin
+
+
+ FSett.Close;
+end;
+
+procedure TFSett.Button1Click(Sender: TObject);
+begin
+ If OpenDialog1.Execute then
+  Edit1.Text:=OpenDialog1.Filename;
+end;
+
+procedure TFSett.Button2Click(Sender: TObject);
+begin
+  If OpenDialog1.Execute then
+  Edit2.Text:=OpenDialog1.Filename;
+end;
 
 end.
