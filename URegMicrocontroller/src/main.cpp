9db3d861117a9aa09eda9829e984cd782c62909d
stanislaumich <staslz42stas@gmail.com>
2023-04-27 22:38:51 +0300

    AUTO FROM HOME 27.04.2023 22:38:50,91

--- URegMicrocontroller/src/main.cpp
+++ URegMicrocontroller/src/main.cpp
@@ -16,63 +16,45 @@
 9 - 
 10- 
 */
-#define pinup 2
-#define pindown 3
-
+#define pinup 3
+#define pindown 2
 #define speed 115200
 
 const int len = 10;
 /*static*/ byte dataArray[len];
 
-int bigstep,smallstep;
-int Udes;
-int Ucur;
-
 void upoff(){
  digitalWrite(pinup,HIGH);
- dataArray[12] = 48;
 }
 void downoff(){
  digitalWrite(pindown,HIGH);
- dataArray[11] = 48;
 }
-
 void upon(){
- digitalWrite(pindown,HIGH);
- dataArray[11] = 48;
  digitalWrite(pinup,LOW);
- dataArray[12] = 49;
 }
 void downon(){
- digitalWrite(pinup,HIGH);
- dataArray[12] = 48;
  digitalWrite(pindown,LOW);
- dataArray[11] = 49;
-}
-
-
-void upstep(){
- upon();
- delay(dataArray[1]*100);
- upoff();
-}
-
-void downstep(){
- downon();
- delay(dataArray[1]*100);
- downoff();
 }
 
 void sendpacket(){
-   Serial.write(dataArray,20);
+   Serial.write(dataArray,len);
 }
 
 void setup() {  
   Serial.begin(speed);
   pinMode(pinup,OUTPUT);
-  upoff();
+  upoff();  
   pinMode(pindown,OUTPUT);
   downoff();
+  /*
+  upon();
+  delay(1000);
+  downon();
+  delay(1000);
+  upoff();
+  delay(1000);
+  downoff();
+  */
 }
 
 void loop() {
@@ -82,17 +64,10 @@ if (Serial.available() > 0) {
     }
 
   switch (dataArray[0]) {
-  case 48:  sendpacket();break;
-  case 49:  sendpacket();break;
-  case 50:  sendpacket();break;
-  case 51:  downoff(); upoff();sendpacket();break;
-  case 52:  downon();sendpacket();break;
-  case 53:  upon();sendpacket();break;
-  
-  case 55:  downstep();sendpacket();break;
-  case 56:  upstep();sendpacket();break;
-
-
+  case 48:  upon();Serial.print("0");break;
+  case 49:  downon();Serial.print("1");break;
+  case 50:  upoff();Serial.print("2");break;
+  case 51:  downoff();Serial.print("3");break;
   }
   // очищаем чобы не слал постоянно а ждал запроса
   dataArray[0]=255;
